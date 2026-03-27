70610ccfe71d408c6960a76e087672eb1cacbc15
bofher <grega.bogdan.v@gmail.com>
2026-03-27 15:31:28 +0200

    swagger added

--- src/main/java/com/ccp/WorkBridge/config/SecurityConfig.java
+++ src/main/java/com/ccp/WorkBridge/config/SecurityConfig.java
@@ -30,6 +30,11 @@ public class SecurityConfig {
                 )
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/auth/**").permitAll()
+                        .requestMatchers(
+                                "/swagger-ui/**",
+                                "/swagger-ui.html",
+                                "/v3/api-docs/**"
+                        ).permitAll()
                         .anyRequest().authenticated()
                 )
                 .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
